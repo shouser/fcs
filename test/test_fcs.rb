33aa276f8c4c72eb7af94f415bb7f733e9227b4b
Steve Houser <steve@tilting.at>
2011-06-06 13:57:20 -0600

    Initial commit to fcs.

---
+++ test/test_fcs.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestFcs < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
